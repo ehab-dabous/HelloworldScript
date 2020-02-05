493ad9533ca2e93b3e8a8dad3a484aa7043fd56d
ehab-dabous <60342764+ehab-dabous@users.noreply.github.com>
2020-02-05 17:15:46 +0200

    Add files via upload

---
+++ tst3.bat
@@ -0,0 +1,8 @@
+@echo off 
+setlocal enableDelayedExpansion 
+for /F "tokens=1" %%x in ('zowe zos-files list all-members "pfp.qa.rexx.mvsixd" --max 15 --u mvsixd --pass sama3406 --host db2a --port 32208') do (
+    echo  %%x)
+PAUSE
+
+
+
