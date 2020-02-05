147541fd1b02f92c3308f98153ae37c16c2b209e
ehab-dabous <60342764+ehab-dabous@users.noreply.github.com>
2020-02-05 17:15:29 +0200

    Delete tst3.bat

--- tst3.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-setlocal enableDelayedExpansion 
-for /F "tokens=1" %%x in ('zos-files list all-members "pfp.qa.rexx.mvsixd" --max 15 --u mvsixd --pass sama3406 --host db2a --port 32208') do (
-    echo  %%x)
-PAUSE
-
-
-
